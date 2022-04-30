0ec18f9f771394683638bd8469ba2ac4f62c1892
joyunju <whdbswn1113@naver.com>
2022-04-30 14:21:50 +0900

    toString()을 재정의

--- src/com/javaex/ex03/Point.java
+++ src/com/javaex/ex03/Point.java
@@ -1,15 +1,26 @@
+// 아래와 같이 출력이 되도록 toString()을 재정의 하세요.(main메소드는 수정하지 않습니다.)
+// [포인트] x:3, y:3 입니다.
+
 package com.javaex.ex03;
 
 public class Point {
 	
+	//필드
 	private int x;
 	private int y;
 	
+	//생성자
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
+	
+	
+	//메소드
 	//toString()을 작성 하세요
-
+	@Override
+	public String toString() {
+		return "[포인트] x:" + x + ", y:" + y + " 입니다.";
+	}
 }
--- src/com/javaex/ex03/PointApp.java
+++ src/com/javaex/ex03/PointApp.java
@@ -1,3 +1,6 @@
+// PointApp.java 실행시 아래와 같이 출력됩니다.
+// com.javaex.ex03.Point@6f2b958e
+
 package com.javaex.ex03;
 
 public class PointApp {
