b77fc1dc8003a557aa8d72b99ab1601353ca975b
joyunju <whdbswn1113@naver.com>
2022-04-30 15:53:50 +0900

    equals(Object obj) 재정의

--- src/com/javaex/ex07/Point.java
+++ src/com/javaex/ex07/Point.java
@@ -1,16 +1,29 @@
 package com.javaex.ex07;
 
 public class Point {
-	
+
+	// 필드
 	private int x;
 	private int y;
-	
+
+	// 생성자
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
-	//equals() 메소드를 작성하세요.
+	// equals() 메소드를 작성하세요.
+	@Override
+	public boolean equals(Object obj) {
 
-	
+		// return super.equals(obj);
+		Point p = (Point) obj;
+
+		if (this.x == p.x) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 }
--- src/com/javaex/ex07/PointApp.java
+++ src/com/javaex/ex07/PointApp.java
@@ -1,3 +1,7 @@
+//아래의 프로그램에서 Point의 정의는 x값이 같으면 같은 Point로 정의합니다.
+//ex, p1=2, 3 p2=2, 12 는 같고,
+// p3=2, 3 p4=5, 3 은 다르다.
+
 package com.javaex.ex07;
 
 public class PointApp {
@@ -14,7 +18,12 @@ public class PointApp {
 		System.out.println(p1.equals(p3));  // (2,3) (5,3) 은 다르다
 		System.out.println(p4.equals(p1));  // p1 과 p4는 같다
 		
-		
+		//재정의 후 결과 값
+//		true
+//		false
+//		false
+//		true
+
 	}
 
 }
