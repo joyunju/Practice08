1ad6471058718ac6835a890115108edbf489ff67
joyunju <whdbswn1113@naver.com>
2022-04-30 14:16:35 +0900

    try~catch문 : IOException

--- src/com/javaex/ex02/FileApp.java
+++ src/com/javaex/ex02/FileApp.java
@@ -1,3 +1,6 @@
+// ex02 문제
+// FileApp.java 코드 main() 메소드 실행시 아래와 같이 예외가 발생합니다.
+
 package com.javaex.ex02;
 
 import java.io.IOException;
@@ -7,7 +10,13 @@ public class FileApp {
 	public static void main(String[] args) {
 
 		// try~catch문을 사용하여 에러를 수정하세요
-		fileRead();
+	//	fileRead();
+		try {
+			fileRead();
+			
+		} catch (Exception e) {
+			System.out.println("파일이 없습니다.");
+		}
 
 	}
 
@@ -15,6 +24,8 @@ public class FileApp {
 	public static void fileRead() throws IOException{
 		throw new IOException(); // 강제로 예외 발생
 	}
+	
+	
 }
 
 
