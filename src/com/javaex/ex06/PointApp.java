503834ac2459c290db28b6eb4d5d5a1b740ee968
joyunju <whdbswn1113@naver.com>
2022-04-30 15:57:23 +0900

    Object Class

--- src/com/javaex/ex06/PointApp.java
+++ src/com/javaex/ex06/PointApp.java
@@ -1,3 +1,5 @@
+//아래의 프로그램 실행시 결과를 예상하고 실행 후 확인해 보세요.
+
 package com.javaex.ex06;
 
 public class PointApp {
@@ -9,12 +11,15 @@ public class PointApp {
 		Point p3 = new Point(5, 3);
 		Point p4 = p1;
 		
-		System.out.println(p1 == p2);
-		System.out.println(p2 == p3);
-		System.out.println(p3 == p4);
-		System.out.println(p4 == p1);		
-		System.out.println(p1.equals(p2));
-		System.out.println(p4.equals(p1));
+		System.out.println(p1 == p2);		//false
+		System.out.println(p2 == p3);		//false
+		System.out.println(p3 == p4);		//false
+		System.out.println(p4 == p1);		//true		
+		System.out.println(p1.equals(p2));	//false
+		System.out.println(p4.equals(p1));	//true	
+		
+		//equals() 메소드가 없는데 실행되는 이유를 말하세요.
+		//최상위 클래스 Object Class로 부처 자동으로 상속받음
 	}
 
 }
